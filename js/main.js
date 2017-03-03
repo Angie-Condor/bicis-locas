f651a697e6e5cbc06ab351a1b9e7cf0048ce42f3
Angie Condor <angiegita1602@gmail.com>
2017-03-03 17:38:52 -0500

    commit de avance

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 function validateForm(){
 	/* Escribe tú código aquí */
+  var name = document.getElementById('name');
+  var lastname = document.getElementById('lastname');
+  var email = document.getElementById('input-email');
+  var password = document.getElementById('input-password');
+  var requeridos = document.getElementsByClassName('form-control');
+  var twitter = document.getElementById('input-social');
+
+  if (requeridos[0].value == "" || requeridos[1].value == "" || requeridos[2].value == "" || requeridos[3].value == ""){
+    alert ("Error: Debe llenar este campo") ;
+  }
+
+  if(name.value == /^[a-zA-Z]*$/){ 
+    alert ("Error: Debe ingresar letras de la a A a la Z.") ;
+  }
 }
