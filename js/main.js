67b65af4f0fb88ae3be24bf4f242913dcace5d57
Angie Condor <angiegita1602@gmail.com>
2017-03-06 01:01:06 -0500

    validación completa

--- js/main.js
+++ js/main.js
@@ -5,28 +5,25 @@ function validateForm(){
   var email = document.getElementById('input-email');
   var password = document.getElementById('input-password');
   var requeridos = document.getElementsByClassName('form-control');
+  var bicicleta = document.getElementById('bicicleta');
   var twitter = document.getElementById('input-social');
-  var letras = /[A-Z]+[a-z]*/;
+  var letras = /^[a-zA-Zñá-ú+\s]*$/;
   var letra1Mayus = /^[A-Z]{1}/;
   var formatoEmail = /\S+@\S+\.\S+/;
 
-  if (requeridos[0].value == "" || requeridos[1].value == "" || requeridos[2].value == "" || requeridos[3].value == ""){
-    alert ("Error:\n Debe llenar los 4 campos obligatoriamente") ;
-  }
-
-  if(name.value != letras.value){
-    alert ("Error:\n Debe ingresar letras de la a A a la Z.") ;
-  } else if(lastname.value != letras.value){
-      alert ("Error:\n Debe ingresar letras de la a A a la Z.") ;
-  } else if(name.value != letra1Mayus.value){
-      alert ("Error:\n Debe ingresar la primera letra de su nombre con mayúscula.") ;
-  } else if(lastname.value != letra1Mayus.value){
-      alert ("Error:\n Debe ingresar la primera letra de su apellido con mayúscula.") ;
-  } else if(email.value != formatoEmail.value){
+  if (requeridos[0].value == "" || requeridos[1].value == "" || requeridos[2].value == "" || requeridos[3].value == "" || bicicleta.value == "0"){
+    alert ("Error:\n Debe llenar los 5 primeros campos obligatoriamente") ;
+  } else if(name.value.search(letras) || lastname.value.search(letras)){
+    alert ("Error:\n Debe ingresar en el Nombre y/o Apellido letras de la A a la Z.") ;
+  } else if(name.value.search(letra1Mayus) || lastname.value.search(letra1Mayus)){
+      alert ("Error:\n Debe ingresar en el Nombre y/o Apellido la primera letra con mayúscula.") ;
+  } else if(email.value.search(formatoEmail)){
       alert ("Error:\n Debe ingresar un e-mail válido.") ;
   } else if (password.value == "password" || password.value == "123456" || password.value == "098754") {
       alert("Error:\n No puede usar esa palabra como password.");
   } else if (password.value.length < 6) {
-      alert("Error:\n Ingresa como mínimo 6 caracteres.");
+      alert("Error:\n Password debe tener como mínimo 6 caracteres.");
+  } else {
+      alert("Los datos son correctos. \n Gracias.");
   }
 }
