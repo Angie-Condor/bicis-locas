8d9202950d8f629a8b0c4b84505b217878b58eb0
Angie Condor <angiegita1602@gmail.com>
2017-03-05 23:39:42 -0500

    commit validación

--- js/main.js
+++ js/main.js
@@ -6,12 +6,17 @@ function validateForm(){
   var password = document.getElementById('input-password');
   var requeridos = document.getElementsByClassName('form-control');
   var twitter = document.getElementById('input-social');
+  var letras = /[A-Z]+[a-z]*/;
 
   if (requeridos[0].value == "" || requeridos[1].value == "" || requeridos[2].value == "" || requeridos[3].value == ""){
-    alert ("Error: Debe llenar este campo") ;
+    alert ("Error: Debe llenar los 4 campos obligatoriamente") ;
   }
 
-  if(name.value == /^[a-zA-Z]*$/){ 
+  if(name.value != letras.value){
+    alert ("Error: Debe ingresar letras de la a A a la Z.") ;
+  }
+
+  if(lastname.value != letras.value){
     alert ("Error: Debe ingresar letras de la a A a la Z.") ;
   }
 }
